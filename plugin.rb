ce259336b932f3c6e82efb770f810e48a137e206
David Taylor <david@taylorhq.com>
2022-11-22 13:47:37 +0000

    Implement discourse-cdn-experiment plugin

--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,94 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
-# version: 0.0.1
+# name: discourse-cdn-experiment
+# about: Allows A/B testing multiple CDN domains
+# version: 1.0
 # authors: Discourse
-# url: TODO
-# required_version: 2.7.0
+# url: https://github.com/discourse/discourse-cdn-experiment
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :cdn_experiment_enabled
+
+module ::CdnExperiment
+  ENV_KEY = "discourse-cdn-experiment-cdn-index"
+
+  def self.perform_gsub(value, env)
+    if value.is_a? String
+      was_html_safe = value.html_safe?
+      value = value.gsub(GlobalSetting.cdn_url, app_cdn_url(env))
+      value = value.gsub(GlobalSetting.s3_cdn_url, s3_cdn_url(env))
+      value = value.html_safe if was_html_safe
+      value
+    elsif value.is_a? Hash
+      value.transform_values { |hash_value| perform_gsub(hash_value, env) }
+    else
+      value
+    end
+  end
+
+  def self.s3_cdn_url(env)
+    s3_cdn_urls[current_cdn_index(env)]
+  end
+
+  def self.app_cdn_url(env)
+    app_cdn_urls[current_cdn_index(env)]
+  end
+
+  def self.current_cdn_index(env)
+    env[ENV_KEY] ||= pick_new_cdn_index
+  end
+
+  def self.pick_new_cdn_index
+    min_array_length = [app_cdn_urls.length, s3_cdn_urls.length].min
+    rand(0...min_array_length)
+  end
+
+  def self.app_cdn_urls
+    [GlobalSetting.cdn_url, *SiteSetting.cdn_experiment_app_cdns.split("|")]
+  end
+
+  def self.s3_cdn_urls
+    [GlobalSetting.s3_cdn_url, *SiteSetting.cdn_experiment_s3_cdns.split("|")]
+  end
+end
 
 after_initialize do
+  reloadable_patch do
+    ApplicationHelper.class_eval do
+      [
+        :script_asset_path,
+        :discourse_stylesheet_preload_tag,
+        :discourse_stylesheet_link_tag,
+        :theme_lookup,
+        :theme_translations_lookup,
+        :theme_js_lookup,
+        :discourse_preload_color_scheme_stylesheets,
+        :discourse_color_scheme_stylesheets,
+        :client_side_setup_data
+      ].each do |method_name|
+        alias_method :"orig_#{method_name}", :"#{method_name}"
+        define_method(method_name) do |*args, **kwargs|
+          result = send(:"orig_#{method_name}", *args, **kwargs)
+          return result if !SiteSetting.cdn_experiment_enabled
+          CdnExperiment.perform_gsub(result, request.env)
+        end
+      end
+    end
+
+    ContentSecurityPolicy::Default.class_eval do
+      alias_method :orig_script_assets, :script_assets
+      def script_assets(*args, **kwargs)
+        entries = orig_script_assets(*args, **kwargs)
+        return entries if !SiteSetting.cdn_experiment_enabled
+        additional_entries = []
+        entries.each do |entry|
+          if entry.include?(GlobalSetting.cdn_url)
+            CdnExperiment.app_cdn_urls[1..].each { |cdn_url| additional_entries << entry.gsub(GlobalSetting.cdn_url, cdn_url) }
+          elsif entry.include?(GlobalSetting.s3_cdn_url)
+            CdnExperiment.s3_cdn_urls[1..].each { |s3_cdn_url| additional_entries << entry.gsub(GlobalSetting.s3_cdn_url, s3_cdn_url) }
+          end
+        end
+        [*entries, *additional_entries]
+      end
+    end
+  end
 end
---
+++ spec/integration/cdn_experiment_spec.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+describe "CDN Experiment" do
+  before do
+    global_setting :s3_cdn_url, "https://original-s3-cdn.example.com"
+    global_setting :s3_bucket, 'mybucket'
+    global_setting :s3_region, 'us-west-1'
+    global_setting :s3_use_iam_profile, true
+
+    set_cdn_url "https://original-app-cdn.example.com"
+
+    SiteSetting.cdn_experiment_enabled = true
+    SiteSetting.cdn_experiment_s3_cdns = "https://new-s3-cdn.example.com"
+    SiteSetting.cdn_experiment_app_cdns = "https://new-app-cdn.example.com"
+    Fabricate(:admin) # Stop the wizard from appearing
+
+    CdnExperiment.stubs(:pick_new_cdn_index).returns(0, 1) # Sequential, not random
+  end
+
+  it "alternates CDNs for static JS assets" do
+    get "/"
+    expect(response.status).to eq(200)
+    doc = Nokogiri::HTML5(response.body)
+    discourse_js = doc.at_css('head script[src*="discourse.js"]')
+    expect(discourse_js["src"]).to eq('https://original-s3-cdn.example.com/assets/discourse.js')
+
+    get "/"
+    expect(response.status).to eq(200)
+    doc = Nokogiri::HTML5(response.body)
+    discourse_js = doc.at_css('head script[src*="discourse.js"]')
+    expect(discourse_js["src"]).to eq('https://new-s3-cdn.example.com/assets/discourse.js')
+  end
+
+  it "alternates CDNs for stylesheet assets" do
+    get "/"
+    expect(response.status).to eq(200)
+    doc = Nokogiri::HTML5(response.body)
+    desktop_css = doc.at_css('link[rel=stylesheet][href*="stylesheets/desktop"]')
+    expect(desktop_css["href"]).to start_with('https://original-app-cdn.example.com/stylesheets/desktop_')
+
+    get "/"
+    expect(response.status).to eq(200)
+    doc = Nokogiri::HTML5(response.body)
+    desktop_css = doc.at_css('link[rel=stylesheet][href*="stylesheets/desktop"]')
+    expect(desktop_css["href"]).to start_with('https://new-app-cdn.example.com/stylesheets/desktop_')
+  end
+
+  context "with theme JS" do
+    before do
+      t = Fabricate(:theme)
+      t.set_field(target: :extra_js, type: :js, name: "discourse/initializers/blah.js", value: "console.log('hello world');")
+      t.save!
+      t.set_default!
+    end
+
+    it "alternates CDNs for theme JS assets" do
+      get "/"
+      expect(response.status).to eq(200)
+      doc = Nokogiri::HTML5(response.body)
+      theme_js = doc.at_css('script[src*="/theme-javascripts/"]')
+      expect(theme_js["src"]).to start_with('https://original-app-cdn.example.com/theme-javascripts/')
+
+      get "/"
+      expect(response.status).to eq(200)
+      doc = Nokogiri::HTML5(response.body)
+      theme_js = doc.at_css('script[src*="/theme-javascripts/"]')
+      expect(theme_js["src"]).to start_with('https://new-app-cdn.example.com/theme-javascripts/')
+    end
+  end
+
+  it "alternates CDNs in the setup data" do
+    get "/"
+    expect(response.status).to eq(200)
+    doc = Nokogiri::HTML5(response.body)
+    setup = doc.at_css('#data-discourse-setup')
+    expect(setup["data-cdn"]).to eq('https://original-app-cdn.example.com')
+    expect(setup["data-s3-cdn"]).to eq('https://original-s3-cdn.example.com')
+
+    get "/"
+    expect(response.status).to eq(200)
+    doc = Nokogiri::HTML5(response.body)
+    setup = doc.at_css('#data-discourse-setup')
+    expect(setup["data-cdn"]).to eq('https://new-app-cdn.example.com')
+    expect(setup["data-s3-cdn"]).to eq('https://new-s3-cdn.example.com')
+  end
+
+  it "includes all CDNs in the CSP" do
+    get "/"
+    expect(response.status).to eq(200)
+    csp = response.headers["Content-Security-Policy"]
+    expect(csp).to include("https://original-app-cdn.example.com")
+    expect(csp).to include("https://new-app-cdn.example.com")
+    expect(csp).to include("https://original-s3-cdn.example.com")
+    expect(csp).to include("https://new-s3-cdn.example.com")
+  end
+end
